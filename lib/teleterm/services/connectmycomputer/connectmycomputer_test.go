adc66582890af466bde1e328b5b57349e696eac0
Rafał Cieślak <rafal.cieslak@goteleport.com>
2023-07-13 18:06:39 +0200

    WIP

--- lib/teleterm/services/connectmycomputer/connectmycomputer_test.go
+++ lib/teleterm/services/connectmycomputer/connectmycomputer_test.go
@@ -16,6 +16,8 @@ package connectmycomputer
 
 import (
 	"context"
+	"errors"
+	"sync"
 	"testing"
 
 	"github.com/gravitational/trace"
@@ -46,8 +48,33 @@ func TestRoleSetupRun_WithNonLocalUser(t *testing.T) {
 	require.True(t, trace.IsBadParameter(err))
 }
 
+func TestRoleSetupRun_UpdateResourcesOnlyOnce(t *testing.T) {
+	roleSetup, err := NewRoleSetup(&RoleSetupConfig{})
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	user, err := types.NewUser("alice")
+	require.NoError(t, err)
+	accessAndIdentity := &mockAccessAndIdentity{user: user, callCounts: make(map[string]int)}
+	certManager := &mockCertManager{}
+
+	_, err = roleSetup.Run(ctx, accessAndIdentity, certManager, &clusters.Cluster{URI: uri.NewClusterURI("foo")})
+	require.NoError(t, err)
+
+	_, err = roleSetup.Run(ctx, accessAndIdentity, certManager, &clusters.Cluster{URI: uri.NewClusterURI("foo")})
+	require.NoError(t, err)
+
+	require.Equal(t, 1, accessAndIdentity.callCounts["UpsertRole"], "expected two runs to update the role only once")
+	require.Equal(t, 1, accessAndIdentity.callCounts["UpdateUser"], "expected two runs to update the user only once")
+}
+
 type mockAccessAndIdentity struct {
-	user types.User
+	user       types.User
+	role       types.Role
+	callCounts map[string]int
+	events &fakeEvents{}
 }
 
 func (m *mockAccessAndIdentity) GetUser(name string, withSecrets bool) (types.User, error) {
@@ -55,23 +82,31 @@ func (m *mockAccessAndIdentity) GetUser(name string, withSecrets bool) (types.Us
 }
 
 func (m *mockAccessAndIdentity) GetRole(ctx context.Context, name string) (types.Role, error) {
+	if m.role != nil {
+		return m.role, nil
+	}
+	return nil, trace.NotFound("role not found")
+}
+
+func (m *mockAccessAndIdentity) UpsertRole(ctx context.Context, role types.Role) error {
+	m.callCounts["UpsertRole"]++
+	m.role = role
+	return nil
+}
+
+// TODO: Watcher in tests?
+func (m *mockAccessAndIdentity) NewWatcher(ctx context.Context, watch types.gatch) (types.Watcher, error) {
 	return nil, trace.NotImplemented("not implemented")
 }
 
-func (m *mockAccessAndIdentity) UpsertRole(context.Context, types.Role) error {
-	return trace.NotImplemented("not implemented")
-}
-
-func (m *mockAccessAndIdentity) NewWatcher(ctx context.Context, watch types.Watch) (types.Watcher, error) {
-	return nil, trace.NotImplemented("not implemented")
-}
-
-func (m *mockAccessAndIdentity) UpdateUser(context.Context, types.User) error {
-	return trace.NotImplemented("not implemented")
+func (m *mockAccessAndIdentity) UpdateUser(ctx context.Context, user types.User) error {
+	m.callCounts["UpdateUser"]++
+	m.user = user
+	return nil
 }
 
 type mockCertManager struct{}
 
 func (m *mockCertManager) ReissueUserCerts(context.Context, client.CertCachePolicy, client.ReissueParams) error {
-	return trace.NotImplemented("not implemented")
+	return nil
 }
